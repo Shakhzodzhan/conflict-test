0b7dc5876d3ebb7552cad658b7f58a11216a220a
Shakhzod <imshakhzod@gmail.com>
2021-02-18 12:00:06 +0100

    first commit

---
+++ src/components/SidebarMenu.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavItem, Button } from 'react-bootstrap';
+import Sidebar from 'react-bootstrap-sidebar';
+
+
+class SidebarMenu extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isVisible: false
+        };
+    }
+
+    updateModal(isVisible) {
+        this.state.isVisible = isVisible;
+        this.forceUpdate();
+    }
+
+    render() {
+        return (
+            <div>
+                <Button bsStyle="primary" onClick={() => this.updateModal(true)}>dsa</Button>
+                <Sidebar side='left' isVisible={this.state.isVisible} onHide={() => this.updateModal(false)}>
+                    <Nav>
+                        <NavItem href="#">Link 1</NavItem>
+                        <NavItem href="#">Link 2</NavItem>
+                        <NavItem href="#">Link 3</NavItem>
+                        <NavItem href="#">Link 4</NavItem>
+                    </Nav>
+                </Sidebar>
+            </div>
+        );
+    }
+};
+
+export default SidebarMenu;
---
+++ src/firebase/base.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+import "firabase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtxQdVwQayRwAi7vO7Ctz_ktioG00mES8",
+  authDomain: "fir-react-auth-50c10.firebaseapp.com",
+  projectId: "fir-react-auth-50c10",
+  storageBucket: "fir-react-auth-50c10.appspot.com",
+  messagingSenderId: "135973558840",
+  appId: "1:135973558840:web:b554ac59f2c2ea6651e4cd",
+  measurementId: "G-NFL55DLK9B"
+};
